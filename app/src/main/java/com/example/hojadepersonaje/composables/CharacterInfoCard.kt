2862fcec3895f0b8059711e85a9796663e61848a
Marco Zanette <mgzanette2002@hotmail.com>
2026-04-06 11:45:31 -0300

    CharacterInfoCard

---
+++ app/src/main/java/com/example/hojadepersonaje/composables/CharacterInfoCard.kt
@@ -0,0 +1,58 @@
+package com.example.hojadepersonaje.composables
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+    fun RowScope.CharacterInfoCard(modifier:Modifier= Modifier, text:String="", onClick:()->Unit){
+        Box(modifier=modifier
+            .padding(1.dp)
+            .border(
+                width = 1.dp,
+                color=MaterialTheme.colorScheme.primary,
+                shape= AbsoluteRoundedCornerShape(8.dp)
+            )
+            .height(140.dp)
+            .padding(2.dp))
+        {
+            Text(
+                text = text,
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(alignment = Alignment.Center)
+                    .clickable(onClick = { onClick })
+            )
+        }
+
+    }
+
+
+    @Preview
+    @Composable
+    private fun info() {
+        Row {
+            CharacterInfoCard(modifier = Modifier, text = "as", onClick = {})
+        }
+        }
+
--- app/src/main/java/com/example/hojadepersonaje/views/characterSheetView.kt
+++ app/src/main/java/com/example/hojadepersonaje/views/characterSheetView.kt
@@ -3,6 +3,8 @@ package com.example.hojadepersonaje.views
 import android.graphics.drawable.shapes.Shape
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.foundation.shape.CircleShape
@@ -17,10 +20,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.hojadepersonaje.characterdata.RPCharacter
+import com.example.hojadepersonaje.composables.CharacterInfoCard
 import com.example.hojadepersonaje.ui.theme.HojaDePersonajeTheme
 
 @Composable
@@ -40,8 +46,14 @@ fun CharacterSheetView(character: RPCharacter,popBackStack:()->Unit){
                 .fillMaxHeight(0.1f)
                 .fillMaxWidth())
             {
-                Box(modifier = Modifier.
-                border(width = 1.dp, color= MaterialTheme.colorScheme.outline, shape = AbsoluteRoundedCornerShape(8.dp))) {
+                Box(modifier = Modifier
+                    .padding(1.dp)
+                    .fillMaxWidth(0.216f)
+                    .border(width = 1.dp, color= MaterialTheme.colorScheme.outline, shape = AbsoluteRoundedCornerShape(8.dp))
+
+                )
+
+                {
                     Surface(
                         shape = CircleShape,
                         color = MaterialTheme.colorScheme.secondaryContainer,
@@ -54,6 +66,7 @@ fun CharacterSheetView(character: RPCharacter,popBackStack:()->Unit){
                     }
                 }
                 Text(text="Nombre del personaje",modifier=Modifier
+                    .padding(1.dp)
                     .fillMaxSize()
                     .border(
                         width = 1.dp,
@@ -65,8 +78,9 @@ fun CharacterSheetView(character: RPCharacter,popBackStack:()->Unit){
 
             Row(modifier=Modifier.fillMaxHeight()) {
                 Column(modifier=Modifier
+                    .padding(1.dp)
                     .fillMaxHeight()
-                    .fillMaxWidth(0.25f)
+                    .fillMaxWidth(0.216f)
                     .border(
                         width = 1.dp,
                         color = MaterialTheme.colorScheme.outline,
@@ -75,14 +89,40 @@ fun CharacterSheetView(character: RPCharacter,popBackStack:()->Unit){
 
                 }
                 Column(modifier=Modifier
+                    .padding(1.dp)
                     .fillMaxSize()
                     .border(
                         width = 1.dp,
                         color = MaterialTheme.colorScheme.outline,
                         shape = AbsoluteRoundedCornerShape(8.dp)
-                    )) {
-                    Row() {
+                    )
 
+                ) {
+                    val infoCardMod=Modifier
+                        .padding(2.dp)
+                        .border(
+                            width = 1.dp,
+                            color=MaterialTheme.colorScheme.primary,
+                            shape= AbsoluteRoundedCornerShape(8.dp)
+                        )
+                        .height(140.dp)
+                        .padding(8.dp)
+                    Row(horizontalArrangement = Arrangement.SpaceEvenly
+                        ,modifier=Modifier.fillMaxSize()) {
+
+                        CharacterInfoCard(
+                            modifier=Modifier.fillMaxWidth(0.5f),
+                            text="Actions",
+                            onClick = {
+
+                            })
+                        CharacterInfoCard(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "Spells",
+                            onClick = {
+
+                            }
+                        )
                     }
                     Row() {
 
